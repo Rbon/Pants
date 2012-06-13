b9973bcce2483706d6adfa6c8bcedee922b515d4
Robin Bisho <rbon@email.com>
2012-06-13 03:53:32 -0700

    Added a config file, so I don't have to update the nick and channel anymore, made the log better, and clean up a LOT.

--- Commands.py
+++ Commands.py
@@ -2,14 +2,14 @@ import string
 import random
 import datetime
 import os
+import socket
 
 
 class Commands():
-    def __init__(self, socket, chan, nick, admin):
+    def __init__(self, chan, nick, admin):
         self.admin = admin
         self.nick = nick
         self.chan = chan
-        self.socket = socket
         self.message = ''
         self.sender = ''
         self.chat = ''
@@ -21,90 +21,90 @@ class Commands():
         self.PONG = ''
 
         self.quitPhrases = [
-            "%s dropped" % (self.nick),
-            "%s has been removed" % (self.nick),
+            '%s dropped' % (self.nick),
+            '%s has been removed' % (self.nick),
             ]
         self.botsnackList = [
-            "nomnomnom",
+            'nomnomnom',
             ]
         self.helloList = [
-            "Hola.",
-            "Heyo.",
-            "Yo.",
-            "Hey __SENDER__.",
-            "Hey there, __SENDER__.",
+            'Hola.',
+            'Heyo.',
+            'Yo.',
+            'Hey __SENDER__.',
+            'Hey there, __SENDER__.',
             ]
         self.explodeList = [
-            "\x01ACTION exploded\x01",
+            '\x01ACTION exploded\x01',
             ]
         self.storyList = [
-            "There once was an ugly barnacle. It was so ugly that everyone died. The end.",
-            "It was a dark and stormy night...",
-            "There was something about ponies... I think I'll let jercos tell you.",
+            'There once was an ugly barnacle. It was so ugly that everyone died. The end.',
+            'It was a dark and stormy night...',
+            'There was something about ponies... I think I\'ll let jercos tell you.',
             ]
         self.lonelyList = [
-            "...with my mouth?",
+            '...with my mouth?',
             ]
         self.eyeList = [
-            "__SENDER__: <.<",
+            '__SENDER__: <.<',
             ]
         self.dropList = [
-            "\x01ACTION dropped it like it's hot\x01"
+            '\x01ACTION dropped it like it\'s hot\x01'
             ]
         self.eightBallList = [
-            "It is certain.",
-            "It is decidedly so.",
-            "Without a doubt.",
-            "Yes - definitely.",
-            "You may rely on it.",
-            "As I see it, yes.",
-            "Most likely.",
-            "Outlook good.",
-            "Signs point to yes.",
-            "Yes.",
-            "Reply hazy, try again.",
-            "Ask again later.",
-            "Better not tell you now.",
-            "Cannot predict now.",
-            "Concentrate and ask again.",
-            "Don't count on it",
-            "My reply is no.",
-            "My sources say no.",
-            "Outlook not so good.",
-            "Very doubtful.",
+            'It is certain.',
+            'It is decidedly so.',
+            'Without a doubt.',
+            'Yes - definitely.',
+            'You may rely on it.',
+            'As I see it, yes.',
+            'Most likely.',
+            'Outlook good.',
+            'Signs point to yes.',
+            'Yes.',
+            'Reply hazy, try again.',
+            'Ask again later.',
+            'Better not tell you now.',
+            'Cannot predict now.',
+            'Concentrate and ask again.',
+            'Don\'t count on it',
+            'My reply is no.',
+            'My sources say no.',
+            'Outlook not so good.',
+            'Very doubtful.',
             ]
         self.danceList = [
-            '\x01ACTION does the Pants Dance'
+            '\x01ACTION does the Pants Dance',
             ]
         self.fartList = [
-            'But I don\'t have intestines!'
+            'Herp a derp.',
             ]
         #list of commands
         self.responseList = {
             'dance':self.danceList,
-            "botsnack":self.botsnackList,
-            "hello":self.helloList,
-            "hello.":self.helloList,
-            "hello?":self.helloList,
-            "hi":self.helloList,
-            "hola":self.helloList,
-            "aloha":self.helloList,
-            "yo":self.helloList,
-            "explode":self.explodeList,
-            "tell me a story":self.storyList,
-            "make me not lonely":self.lonelyList,
-            ">.>":self.eyeList,
-            "drop":self.dropList,
+            'botsnack':self.botsnackList,
+            'hello':self.helloList,
+            'hello.':self.helloList,
+            'hello?':self.helloList,
+            'hi':self.helloList,
+            'hola':self.helloList,
+            'aloha':self.helloList,
+            'yo':self.helloList,
+            'explode':self.explodeList,
+            'tell me a story':self.storyList,
+            'make me not lonely':self.lonelyList,
+            '>.>':self.eyeList,
+            'drop':self.dropList,
             'fart':self.fartList,
             }
 
     def Run(self):
         running = None
         while running == None:
-            self.chat = self.socket.makefile().readline()
+            self.chat = socket.socket.makefile().readline()
             if self.chat.find('PING') == 0:
                 self.PONG = self.chat[self.chat.find('PING')+4:]
-                self.socket.send('PONG' + self.PONG)
+                socket.socket.send('PONG' + self.PONG)
             self.sender = self.chat[1:self.chat.find('!')]
             self.message = self.chat[self.chat.find(self.chan) + len(self.chan) + 2:len(self.chat) - 2]
             if self.chat.find('PRIVMSG') != -1:
@@ -119,7 +119,7 @@ class Commands():
                         if self.responseList.has_key(command):
                             self.Respond(command)
                             #check for a question mark
-                        elif command.find("?") != -1:
+                        elif command.find('?') != -1:
                             try:
                                 randomLen = len(self.eightBallList)-1
                                 exactResponse = self.eightBallList[random.randint(0, randomLen)]
@@ -128,16 +128,16 @@ class Commands():
                                     return      
                     except TypeError:
                         running = self.commandList[command]()
-            elif self.chat.find("NOTICE") == -1:
-                if self.chat.find("JOIN")!=-1:
-                    self.Log(self.sender, "\x01ACTION joined "+self.chan)
-                elif self.chat.find("QUIT")!=-1:
-                    messageStart=self.chat.find(":Quit:")+7
+            elif self.chat.find('NOTICE') == -1:
+                if self.chat.find('JOIN')!=-1:
+                    self.Log(self.sender, '\x01ACTION joined '+self.chan)
+                elif self.chat.find('QUIT')!=-1:
+                    messageStart=self.chat.find(':Quit:')+7
                     self.message = self.chat[messageStart:]
-                    self.Log(self.sender, "\x01ACTION quit "+self.chan+": "+self.message)
-                elif self.chat.find("PART")!=-1:
-                    self.Log(self.sender, "\x01ACTION parted "+self.chan+": "+self.message)
-                elif self.chat.find("ACTION") != -1:
+                    self.Log(self.sender, '\x01ACTION quit '+self.chan+': '+self.message)
+                elif self.chat.find('PART')!=-1:
+                    self.Log(self.sender, '\x01ACTION parted '+self.chan+': '+self.message)
+                elif self.chat.find('ACTION') != -1:
                     self.message=chat[messageStart+len(self.chan)+10:]
                     self.Log(self.sender,self.message[0:len(self.message)-2])
         return running
@@ -150,12 +150,12 @@ class Commands():
         self.Send(exactResponse)
 
     def Log(self, sender, message):
-        if message.find("\x01ACTION ") == 0:
+        if message.find('\x01ACTION ') == 0:
             message = message[8:]
         else:
-            sender = "<"+sender+">"
+            sender = '<'+sender+'>'
         self.now = datetime.datetime.today()
-        line = "["+self.now.strftime("%H:%M")+"] "+sender+" "+message
+        line = self.now.strftime('[%Y-%m-%d][%H:%M]')+sender+' '+message
         self.logFile = open('log.txt', 'a')
         self.logFile.write(line+'\n')
         self.logFile.close()
@@ -176,15 +176,15 @@ class Commands():
         
     def Send(self, token):
         self.Log(self.nick, token)
-        self.socket.send('PRIVMSG %s :%s\r\n' % (self.chan, token))
+        socket.socket.send('PRIVMSG %s :%s\r\n' % (self.chan, token))
 
     def Quit(self):
         if self.sender == self.admin:
             randomLen = len(self.quitPhrases)-1
             exactResponse = self.quitPhrases[random.randint(0, randomLen)]
             quitResponse = 'QUIT :'+exactResponse+'\r\n'
-            self.socket.send(quitResponse)
-            self.Log(self.nick, "quit "+self.chan+": "+exactResponse)
+            socket.socket.send(quitResponse)
+            self.Log(self.nick, 'quit '+self.chan+': '+exactResponse)
             return 'quit'
         else:
             self.Send(self.sender + ': you don\'t have permission, dude.')
--- pants.py
+++ pants.py
@@ -4,37 +4,40 @@ import socket
 
 class Core():
     def __init__(self):
-        self.host = 'mindjail.subluminal.net'
-        self.port = 6667
-        self.nick = 'Pants'
-        self.ident = 'Pants'
-        self.realname = 'Pants'
-        self.chan = '#thoseguys'
-        self.admin = 'Rbon'
-        self.passwd = open('passwd.txt', 'r').read()
-        self.socket = None
-
-    
+        self.configFile = open('config.txt', 'r').readlines()
+        self.nick = self.configFile[0].split()[1]
+        self.host = self.configFile[1].split()[1]
+        self.port = int(self.configFile[2].split()[1])
+        self.admin = self.configFile[3].split()[1]
+        self.chan = self.configFile[4].split()[1]
+        self.ident = self.nick
+        self.realname = self.nick
+        try:
+            self.passwd = self.configFile[5].split()[1]
+        except IndexError:
+            self.passwd = None
+        socket.socket = socket.socket()
+        socket.socket.connect((self.host, self.port))
+        socket.socket.send('nick '+self.nick+'\r\n')
+        socket.socket.send('USER '+self.ident+' '+self.host+' derp :'+self.realname+'\r\n')
+        if self.passwd != None:
+            socket.socket.send('PRIVMSG NickServ identify '+self.passwd+'\r\n')
+        socket.socket.send('JOIN '+self.chan+'\r\n')
+        
     def Run(self):
         ret = ''
+        join = True
         while ret != 'quit':
-            commands = Commands.Commands(self.socket, self.chan, self.nick, self.admin)
+            commands = Commands.Commands(self.chan, self.nick, self.admin)
+            if join == True:
+                commands.Log(self.nick, 'joined '+self.chan)
+                join = False
             ret = commands.Run()
             if (ret != 'quit'):
                 reload(Commands)
                 commands.SayReloaded()
 
-    def OpenSocket(self):
-        self.socket = socket.socket()
-        self.socket.connect((self.host, self.port))
-        self.socket.send('nick %s\r\n' % (self.nick))
-        self.socket.send('USER %s %s bla :%s\r\n' % (self.ident, self.host, self.realname))
-        self.socket.send('PRIVMSG NickServ identify '+self.passwd+'\r\n')
-        self.socket.send('JOIN %s\r\n' % (self.chan))
-
-
 core = Core()
-core.OpenSocket()
 core.Run()
-core.socket.shutdown(socket.SHUT_RDWR)
-core.socket.close()
+socket.socket.shutdown(socket.SHUT_RDWR)
+socket.socket.close()
